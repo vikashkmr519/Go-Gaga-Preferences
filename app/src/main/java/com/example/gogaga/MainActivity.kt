dc875fb10246008404802696751655b9d6f5c170
vikashkmr519 <vikashkmr519@gmail.com>
2020-05-27 12:35:10 +0530

    Preferences screen for Go gaga

---
+++ app/src/main/java/com/example/gogaga/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.gogaga
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.akaita.android.circularseekbar.CircularSeekBar
+import com.akaita.android.circularseekbar.CircularSeekBar.OnCircularSeekBarChangeListener
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.NumberFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+         }
+}
